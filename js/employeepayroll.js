c2d8215c3a988ca325053a04d794d898d58cd18d
Madhuri <harshupatil0301@gmail>
2021-07-21 23:44:09 +0530

    [Madhuri]Refactor : Validiating Name in The Form

--- js/employeepayroll.js
+++ js/employeepayroll.js
@@ -4,3 +4,13 @@ output.textContent=salary.value;
 salary.addEventListener('input',function (){
     output.textContent =salary.value;
 });
+
+const text = document.querySelector('#name');
+const textError = document.querySelector('#text-error');
+text.addEventListener('input',function (){
+    let namRegex = new RegExp('^[A-Z][a-zA-z\\s]{2,}$');
+    if(namRegex.test(text.value))
+        textError.textContent = "";
+    else
+        textError.textContent = "Incorrect Name!! It Should Start With Capital Letter";
+});
